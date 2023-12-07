33fc0f9abdc1aa549dd1febced2203506389b89e
Petar Cevriz <pcevriz95@gmail.com>
2023-12-07 18:51:56 +0100

    Add routing to webapp

--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// Add app routing
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// Add login page
+// Add home page
+// Add interceptor
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  { path: "/", element: <h1>Hello World</h1> },
+  { path: "/login", element: <h1>Login Page</h1> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
